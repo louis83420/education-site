e9e18ddf69c5cdb42598bd072f09ec46756f2924
Joy <louis83420@gmail.com>
2024-09-03 11:36:54 +0800

    新增wlecome.style 和添加metablade維持代碼整潔

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    @include('layouts.meta')
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
@@ -23,7 +18,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    首頁
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
---
+++ resources/views/layouts/meta.blade.php
@@ -0,0 +1,25 @@
+<!-- resources/views/layouts/meta.blade.php -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<!-- Description -->
+<meta name="description" content="歡迎來到 Joy 自學中心，這裡提供豐富的學習資源，幫助你在學習旅程中不斷進步。">
+
+<!-- Keywords -->
+<meta name="keywords" content="Joy自學中心, 自學, 線上課程, 學習資源">
+
+<!-- Author -->
+<meta name="author" content="Joy自學中心">
+
+<!-- Copyright -->
+<meta name="copyright" content="© 2024 Joy自學中心. All rights reserved.">
+
+<!-- Open Graph -->
+<meta property="og:type" content="website">
+<meta property="og:title" content="Joy自學中心">
+<meta property="og:description" content="歡迎來到 Joy 自學中心，這裡提供豐富的學習資源，幫助你在學習旅程中不斷進步。">
+<meta property="og:image" content="{{ env('APP_URL') }}/images/image_me.jpg">
+<meta property="og:url" content="{{ env('APP_URL') }}">

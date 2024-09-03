93b60ca372ea8d3b84e9e10f1c2f5b1742c670c2
Joy <louis83420@gmail.com>
2024-09-03 17:15:39 +0800

    修復LoginController BUG

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use App\Models\User;
+
 use Socialite; // 导入 Socialite
 
 class LoginController extends Controller

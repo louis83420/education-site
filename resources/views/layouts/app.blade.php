eb001ee8af81a40632e4ff63cf519f399793874b
Joy <louis83420@gmail.com>
2024-09-05 13:20:16 +0800

    新增用戶可見自己剩餘平台點數

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,6 +50,8 @@
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
+                                <!-- 顯示用戶點數 -->
+                                <span class="text-info ms-2">(點數: {{ Auth::user()->points }})</span>
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
